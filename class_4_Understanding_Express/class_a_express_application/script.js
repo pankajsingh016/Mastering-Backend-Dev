f69aee7c9b0bab242d378bdefc8e76bc2ce1c8aa
Pankaj Singh Kanyal <pankrj123@gmail.com>
2025-06-28 16:43:13 +0530

    completed 4th class on express

--- class_4_Understanding_Express/class_a_express_application/script.js
+++ class_4_Understanding_Express/class_a_express_application/script.js
@@ -1,12 +1,28 @@
-const express = require('express')
+const express = require("express");
 
 const app = express();
 
+// routes /
 
-app.get("/",(req,res)=>{
-    console.log('Hello-World');
-    res.send('Hello World');
+app.use(function (req, res, next) {
+  console.log("middleware chala");
+  next();
+});
+
+app.get("/", (req, res) => {
+  res.send("Champion mera anuj");
+});
+
+app.get("/profile", (req, res,next) => {
+    return next(new Error("Something Went Wrong"));
+    res.send("Champion uska coach");
+
+});
+
+
+app.use((err,req,res,next)=>{
+    console.error(err.stack);
+    res.status(500).send('Something broke!');
 })
-
 
 app.listen(3000);
