5fb8bcc7cb1d16a33080a485baacc31babfdc2d6
Pankaj Singh Kanyal <pankrj123@gmail.com>
2025-06-28 18:44:14 +0530

    completed 5th class on express

--- class_5_Form_handling_and_more/script.js
+++ class_5_Form_handling_and_more/script.js
@@ -1,1 +1,4 @@
 // form handling and working witht the forms
+// handling process of form and data from frontend-lib, templating engines, we still handle it in the backend process
+
+// session, cookie 
