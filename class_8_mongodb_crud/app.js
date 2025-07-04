b17ee72e4c66006b95dd61e676cf2eb1229f3523
Pankaj Singh Kanyal <pankrj123@gmail.com>
2025-07-04 19:19:01 +0530

    mongodb update command userModel.findOneAndUpdate()

--- class_8_mongodb_crud/app.js
+++ class_8_mongodb_crud/app.js
@@ -1,37 +1,52 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const path = require('path');
-const userModel = require('./models/user');
+const path = require("path");
+const userModel = require("./models/user");
 
-app.set("view engine","ejs")
+app.set("view engine", "ejs");
 app.use(express.json());
-app.use(express.urlencoded({extended:true}));
-app.use(express.static(path.join(__dirname,'public')));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, "public")));
 
-app.get('/',(req,res)=>{
-    res.render('index');
-})
+app.get("/", (req, res) => {
+  res.render("index");
+});
 
-app.get('/read',async (req,res)=>{
-    let allusers = await userModel.find();
-    res.render('read',{users:allusers});
-})
+app.get("/read", async (req, res) => {
+  let allusers = await userModel.find();
+  res.render("read", { users: allusers });
+});
 
-app.get('/delete',async(req,res){
-    let users = await userModel.findOneAndDelete({_id:req.params.id});
-    res.redirect('/read')
-})
+app.get("/delete/:userid", async (req, res) => {
+  let users = await userModel.findOneAndDelete({ _id: req.params.userid });
+  res.redirect("/read");
+});
 
-app.post('/create',async (req,res)=>{
-    let createUser = await userModel.create({
-        name:req.body.name,
-        email:req.body.email,
-        image_url:req.body.image_url
-    });
-    console.log('user added');
-    res.redirect('/read');
-})
+app.get("/edit/:userid", async (req, res) => {
+  let user = await userModel.findOne({ _id: req.params.userid });
+  res.render("edit", { user });
+});
 
 
+app.post("/update/:userid", async (req, res) => {
+  let { image_url, name, email } = req.body;
+  let user = await userModel.findOneAndUpdate(
+    { _id: req.params.userid },
+    { name: name, email: email, image_url: image_url },
+    {new:true}
+  );
+  res.redirect('/read');
+});
+
+
+app.post("/create", async (req, res) => {
+  let createUser = await userModel.create({
+    name: req.body.name,
+    email: req.body.email,
+    image_url: req.body.image_url,
+  });
+  console.log("user added");
+  res.redirect("/read");
+});
 
 app.listen(3000);
