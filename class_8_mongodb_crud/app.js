7b0e919a1dc4fe149814380a5a5f8cbc773dbfca
Pankaj Singh Kanyal <pankrj123@gmail.com>
2025-07-04 16:54:56 +0530

    mongodb create read update

---
+++ class_8_mongodb_crud/app.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+const userModel = require('./models/user');
+
+app.set("view engine","ejs")
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+app.use(express.static(path.join(__dirname,'public')));
+
+app.get('/',(req,res)=>{
+    res.render('index');
+})
+
+app.get('/read',async (req,res)=>{
+    let allusers = await userModel.find();
+    res.render('read',{users:allusers});
+})
+
+app.get('/delete',async(req,res){
+    let users = await userModel.findOneAndDelete({_id:req.params.id});
+    res.redirect('/read')
+})
+
+app.post('/create',async (req,res)=>{
+    let createUser = await userModel.create({
+        name:req.body.name,
+        email:req.body.email,
+        image_url:req.body.image_url
+    });
+    console.log('user added');
+    res.redirect('/read');
+})
+
+
+
+app.listen(3000);
