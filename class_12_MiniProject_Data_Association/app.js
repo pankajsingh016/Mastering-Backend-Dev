971839f976c0c9fb9ffb7fa7a046f6e80295ced7
Pankaj Singh Kanyal <pankrj123@gmail.com>
2025-07-08 11:11:55 +0530

    remove error and make code functional

--- class_12_MiniProject_Data_Association/app.js
+++ class_12_MiniProject_Data_Association/app.js
@@ -1,47 +1,45 @@
 const express = require("express");
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
-
+const cookieParser = require("cookie-parser");
 const app = express();
-
 const userModel = require("./models/user");
 const postModel = require("./models/post");
 
 app.set("view engine", "ejs");
 app.use(express.json());
 app.use(express.urlencoded({ entended: true }));
+app.use(cookieParser());
 
 app.get("/", (req, res) => {
   res.render("index");
 });
 
-app.post("/create", (req, res) => {
-  res.send("HEY");
-});
 
 app.post("/register", async (req, res) => {
   let { email, password, username, name, age } = req.body;
   let user = await userModel.findOne({ email });
+  console.log(user);
 
   if (user) {
     return res.status(500).send("User already registered");
   }
 
-  bcrypt.genSalt(10, (err, salt) => {
-    bcrypt.hash(password, salt, async (err, hash) => {
-      let user = await userModel.create({
-        username: username,
-        name: name,
-        password: hash,
-        email: email,
-        age: age,
-      });
-      const token = jwt.sign({ email: email, userid: user._id }, "shazam");
-      res.cookie("token", token);
-      res.send("Registered");
-    });
+  const salt = await bcrypt.genSalt(10);
+  const hash = await bcrypt.hash(password, salt);
+
+  let newuser = await userModel.create({
+    username: username,
+    name: name,
+    password: hash,
+    email: email,
+    age: age,
   });
+  const token = jwt.sign({ email: email, userid: newuser._id }, "shazam");
+  res.cookie("token", token);
+  res.send("Registered");
 });
+// res.redirect("/");
 
 app.get("/login", async (req, res) => {
   res.render("login");
@@ -49,7 +47,7 @@ app.get("/login", async (req, res) => {
 
 app.post("/login", async (req, res) => {
   let { email, password } = req.body;
-  let user = userModel.findOne({ email });
+  let user = await userModel.findOne({ email });
 
   if (!user) return res.status(500).send("Something went wrong");
 
@@ -69,7 +67,7 @@ app.get("/logout", (req, res) => {
 
 app.get("/profile", Isloggedin, (req, res) => {
   console.log(req.user);
-  res.render("login");
+  res.send("Logged in using token from protected routes");
 });
 
 // middleware for protected routes
@@ -77,7 +75,8 @@ function Isloggedin(req, res, next) {
   if (req.cookies.token === "") {
     res.status(404).redirect("/login");
   } else {
-    const data = jwt.verify(token, "shazam");
+    console.log(req.cookies.token);
+    const data = jwt.verify(req.cookies.token, "shazam");
     req.user = data;
     next();
   }
--- class_12_MiniProject_Data_Association/models/user.js
+++ class_12_MiniProject_Data_Association/models/user.js
@@ -7,7 +7,7 @@ const userSchema = mongoose.Schema({
   age: Number,
   email: String,
   password: String,
-  posts: [{ type: mongoose.Schema.Types.ObjectId, ref: "post" }],
+  posts: [{ type: mongoose.Schema.Types.ObjectId, ref: 'post' }],
 });
 
 module.exports = mongoose.model("user", userSchema);
